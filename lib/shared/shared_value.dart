4f44abcaf5ec75f05ecff29a6b692a0dc705669d
nikenprecilia <nikenprecilia11@gmail.com>
2023-03-24 23:08:07 +0700

    first commit

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = "ff9e95cde393114f3f31aa565f16998b";
+String imageBaseURL = "https://image.tmdb.org/t/p/"; 
+
+PageEvent prevPageEvent;
+File imageFileToUpload;
